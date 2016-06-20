df9eb0d1178f8b54d1cd99c04b0a7f60fb7762a9
paula-white <paula-white@hotmail.com>
2016-06-21 07:35:16 +1000

    before animation

---
+++ main.js
@@ -0,0 +1,56 @@
+// var wh = window.innerHeight,
+// 	$iphone = $('.iphone'),
+// 	$innerS1 = $('.innerS1'),
+// 	$innerS2 = $('.innerS2'),
+// 	$innerS3 = $('.innerS3'),
+// 	$innerS4 = $('.innerS4'),
+// 	$screenHat = $('.screenHat'),
+// 	$screenA = $('.screenA'),
+// 	$screenB = $('.screenB'),
+// 	$screenC = $('.screenC');
+
+
+// var wh = window.innerHeight,
+//   $container = $('.section'),
+// 	$sectionone = $('.sectionone'),
+// 	$sectiontwo = $('.sectiontwo'),
+// 	$sectionthree = $('.sectionthree'),
+// 	$sectionfour = $('.sectionfour'),
+// 	$pic1 = $('.pic1'),
+// 	$pic2 = $('.pic2'),
+// 	$pic3 = $('.pic3')
+//   $pic4 = $('.pic4');
+//
+// 	// init
+// 	var ctrl = new ScrollMagic.Controller({
+// 	    globalSceneOptions: {
+// 	        triggerHook: 'onLeave'
+// 	    }
+// 	});
+//   // init
+// 	var ctrl = new ScrollMagic.Controller({
+// 	    globalSceneOptions: {
+// 	        triggerHook: 'onLeave'
+// 	    }
+// 	});
+//
+// 	// Create scene
+// 	$("section").each(function() {
+//
+// 		new ScrollMagic.Scene({
+// 			triggerElement: this,
+// 			duration: '70%'
+// 		})
+// 		.setPin(this)
+// 		.addTo(ctrl);
+//
+// 	});
+//
+//   var tl = new TimelineMax({repeat:-1})
+// tl.to($pic2, 1, { ease:Back.easeInOut })
+//   .to($pic3, 0.5, {  opacity:0 , ease:Power1.easeInOut });
+//
+//
+//   var t4 = new TimelineMax({repeat:-1})
+// tl.to($pic4, 1, { ease:Back.easeInOut })
+//   .to($pic4, 0.5, {  opacity:0 , ease:Power1.easeInOut });
